a8c71b8288f1eaf513dfcb8e8357de85cbc378f4
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-03-06 13:18:49 +0800

    Created Views, and added URL patterns
    # TODO: The templates are just placeholders for now, I haven't populated the database with anything yet

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import product_detail, product_list
 
 urlpatterns = [
-
+    path('items',product_list,name="product_list"),
+    path('item/<int:num>/',product_detail,name="product-detail")
 ]
 
 app_name = "merchstore"
