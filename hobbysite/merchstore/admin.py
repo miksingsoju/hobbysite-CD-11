6c1df3468dfe8c7192ee4d549cd20128ac9eca38
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-03-06 13:18:57 +0800

    Added Admin.py

--- hobbysite/merchstore/admin.py
+++ hobbysite/merchstore/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+# app/admin.py
+from .models import Product, ProductType
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    model = ProductType
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+
+# registering the model and the admin is what tells
+# Django that admin pages must be generated for the models specified
+admin.site.register(ProductType, ProductTypeAdmin)
+admin.site.register(Product, ProductAdmin)
