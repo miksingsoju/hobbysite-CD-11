c6d2c29e848d4a7035cf316db0ae71dba9a3126a
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-03-06 11:59:51 +0800

    Created ProductType and Product models
    #TODO: Products should be sorted by name in ascending order

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class ProductType(models.Model):
+    Name = models.CharField(max_length=255)
+    Description = models.TextField()
+    #TODO: Product Types should be sorted by name in ascending order
+
+class Product(models.Model):
+    Name = models.CharField(max_length=255)
+    ProductType = models.ForeignKey(ProductType,on_delete=models.SET_NULL)
+    Description = models.TextField
+    Price = models.DecimalField(decimal_places=2)
+    #TODO: Products should be sorted by name in ascending order
+
+
+
+
