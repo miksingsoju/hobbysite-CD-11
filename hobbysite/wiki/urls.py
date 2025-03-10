dac0e77c45717a313aacdae8cd0dd2a755750afb
amantenoah <111347849+amantenoah@users.noreply.github.com>
2025-03-10 15:18:16 +0800

    initial commit for branch + creation of working urls

--- hobbysite/wiki/urls.py
+++ hobbysite/wiki/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name="homepage"),
+    path('articles', views.article, name ="article_index"),
+    path('article/<int:num>', views.article_detail, name="article_detail")
 ]
 
 app_name = "wiki"
--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def homepage(request):
     return render(request, "homepage.html")
+
+def article(request):
+    return HttpResponse("Work in progress hehe")
+
+def article_detail(request, num=1):
+    return HttpResponse(f'Work in progress hehe: {num}')
