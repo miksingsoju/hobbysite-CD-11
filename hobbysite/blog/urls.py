1434d0f865bbd088a6be2fa49ff100178b5b30e2
newonathe17 <ethanowen0117@gmail.com>
2025-03-11 13:30:05 +0800

    Add blog templates (article_list.html & article_detail.html) extending base.html and Fix 404 at /blog by adding a default route/redirect in blog/urls.py

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='articles', permanent=False), name='blog-redirect'),
     path('articles', views.article_list, name='article_list'),
     path('article/<int:article_id>', views.article_detail, name='article_detail'),
 ]
