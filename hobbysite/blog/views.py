8d842cf4db9dd47ed02029766c53647a77680b21
newonathe17 <ethanowen0117@gmail.com>
2025-03-13 12:36:59 +0800

    populated the database with articles, made a new view and urls to make an article_by_category views, and fixed some errors and wrong name conventions

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Article
+from .models import Article, ArticleCategory
 
 def article_list(request):
-    articles = Article.objects.all()
-    return render(request, 'blog/article_list.html', {'articles': articles})
+    categories = ArticleCategory.objects.all()  # Retrieve all categories
+    return render(request, 'blog/article_list.html', {
+        'categories': categories,
+    })
+
+def articles_by_category(request, category_id):
+    category = get_object_or_404(ArticleCategory, id=category_id)
+    articles = Article.objects.filter(category=category)
+    return render(request, 'blog/articles_by_category.html', {
+        'category': category,
+        'articles': articles,
+    })
 
 def article_detail(request, article_id):
     article = get_object_or_404(Article, id=article_id)
