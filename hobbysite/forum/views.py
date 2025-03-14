80a50af8be08c9dd4374d722869a692c5ae649d9
amantenoah <111347849+amantenoah@users.noreply.github.com>
2025-03-14 11:06:40 +0800

    few quality of life changes, made a few edits on templates for a bit more consistency

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
 def apology(request):
-    return HttpResponse("Our 5th group member will work on this at their own time, sorry!")
+    return HttpResponse("Our 5th group member will work on this at their own time, sorry! <a href='/'>Go Back</a>")
