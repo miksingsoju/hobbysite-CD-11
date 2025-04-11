625d1bed90b62e98b30e8d0226e16da62df305b2
floodlight <go.miguel65@gmail.com>
2025-04-11 19:34:21 +0800

    fill in forum model fields and sorting, remove comment

--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -1,21 +1,28 @@
 from django.db import models
 
-# PostCategory
-# Name - max length is 255 characters
-# Description - text field
-# Categories should be sorted by name in ascending order
-
-# Post
-# Title - max length is 255 characters
-# Category - foreign key to PostCategory, sets to NULL when deleted
-# Entry - text field
-# Created On - datetime field, only gets set when the model is created
-# Updated On - datetime field, always updates on last model update
-# Posts should be sorted by the date it was created, in descending order
-
 class PostCategory(models.Model):
-    pass
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Post Category'
+        verbose_name_plural = 'Post Categories'
 
 class Post(models.Model):
-    pass
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        PostCategory, 
+        on_delete=models.SET_NULL, 
+        null=True,
+        blank=True
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
 
