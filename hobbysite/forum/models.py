05863fa0128dad849aa509340af6c0859930d814
floodlight <go.miguel65@gmail.com>
2025-04-11 19:23:49 +0800

    create new models, add instructions comment for models

--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# PostCategory
+# Name - max length is 255 characters
+# Description - text field
+# Categories should be sorted by name in ascending order
+
+# Post
+# Title - max length is 255 characters
+# Category - foreign key to PostCategory, sets to NULL when deleted
+# Entry - text field
+# Created On - datetime field, only gets set when the model is created
+# Updated On - datetime field, always updates on last model update
+# Posts should be sorted by the date it was created, in descending order
+
+class PostCategory(models.Model):
+    pass
+
+class Post(models.Model):
+    pass
+
