095cecec194c25bb83a6ca89bfcbcb4f4c7fdbb6
floodlight <go.miguel65@gmail.com>
2025-04-14 14:26:53 +0800

    create superuser, fix base.html links to forum, create new html templates for forum

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import post_list, post_detail
 
 urlpatterns = [
-    path('threads/', post_list, name='threads'),
+    path('threads', post_list, name='threads'),
     path('thread/<int:num>/', post_detail, name='thread'),
 ]
 
