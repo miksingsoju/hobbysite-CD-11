4feec64eee48dd2b34a1d01ba821901638d06213
floodlight <go.miguel65@gmail.com>
2025-04-14 14:06:01 +0800

    replace temp apology references in urls.py

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import apology
+from .views import post_list, post_detail
 
 urlpatterns = [
-    path('threads/', apology, name='threads'),
-    path('thread/<int:num>/', apology, name='thread'),
+    path('threads/', post_list, name='threads'),
+    path('thread/<int:num>/', post_detail, name='thread'),
 ]
 
 app_name = "forum"
