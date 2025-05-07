c860b21fda36b33922e85374322d7ae909296150
amantenoah <111347849+amantenoah@users.noreply.github.com>
2025-05-07 12:31:15 +0800

    updated urls and organized views

--- hobbysite/wiki/urls.py
+++ hobbysite/wiki/urls.py
@@ -4,8 +4,10 @@ from . import views
 # I made homepage tempate at the start, storing here for easy access
 urlpatterns = [
     path('', views.homepage, name="homepage"),
-    path('articles', views.articles, name ="article_index"),
-    path('article/<int:num>', views.article_detail, name="article_detail")
+    path('articles/', views.articles, name ="article_index"),
+    path('article/<int:num>/', views.article_detail, name="article_detail"),
+    path('article/add/', views.new_article, name="add_article"),
+    path('article/<int:num>/edit/', views.edit_article, name="edit_article")
 ]
 
 app_name = "wiki"
--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Article, ArticleCategory
+# For debugging only
+from django.http import HttpResponse
 
 # Storing homepage here since I coded it, prevents the need for a homepage app
 def homepage(request):
@@ -15,3 +17,9 @@ def articles(request):
 def article_detail(request, num=1):
     article = Article.objects.get(pk=num)
     return render(request, "article_detail.html", {"article": article})
+
+def add_article(request):
+    return HttpResponse("Hehe still under construction")
+
+def edit_article(request, num=1):
+    return HttpResponse("Hehe still under construction")
