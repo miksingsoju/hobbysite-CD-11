2c5c71c32bafa5085245dbbc16fc59643884a32a
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-05-11 01:05:43 +0800

    Updated and added new urls for the new views

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
-from .views import product_detail, product_list
+from .views import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('',product_list,name="product_list"), #index page
-    path('items',product_list,name="product_list"),
-    path('item/<int:num>/',product_detail,name="product-detail")
+    path('items/',product_list, name="product_list" ),
+    path('item/<int:num>/', product_detail, name="product_detail"),
+    path('item/add/', product_create, name="product_create"),
+    path('item/<int:num>/edit/', product_update, name="product_update"),
+    path('cart/',cart, name="cart"),
+    path('transactions/', transactions, name="transactions"),
+
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+
+
+    
 ]
 
 app_name = "merchstore"
