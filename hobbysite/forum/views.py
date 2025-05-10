f04d4d7c2cb2faddf8fb435e9a6f9caf6a4fb906
floodlight <go.miguel65@gmail.com>
2025-05-11 03:59:46 +0800

    rename templates and change all name refs to templates in views

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -4,7 +4,7 @@ from .models import ThreadCategory, Thread
 def thread_list(request):
     categories = ThreadCategory.objects.all()
     ctx = {'categories': categories,}
-    return render(request, 'post_list.html', ctx)
+    return render(request, 'thread_list.html', ctx)
 
 def threads_by_category(request, category_id):    
     category = ThreadCategory.objects.filter(id=category_id).first()
@@ -13,9 +13,9 @@ def threads_by_category(request, category_id):
         'category': category,
         'threads': threads,
         }
-    return render(request, 'posts_by_category.html', ctx)
+    return render(request, 'threads_by_category.html', ctx)
 
 def thread_detail(request, thread_id):
     thread = Thread.objects.filter(id=thread_id).first()
     ctx = {'thread': thread}
-    return render(request, 'post_detail.html', ctx)
+    return render(request, 'thread_detail.html', ctx)
