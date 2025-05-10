c08d00b87a3e6b75416d8751e445e23a54807821
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-05-11 01:03:54 +0800

    Minor changes to Profile Model

--- hobbysite/user_management/models.py
+++ hobbysite/user_management/models.py
@@ -2,8 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     display_name = models.CharField(max_length=63)
     email_address = models.EmailField(unique=True)
     # Added for fun
     bio = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.display_name
