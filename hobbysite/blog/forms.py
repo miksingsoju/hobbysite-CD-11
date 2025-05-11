3cf8d7f8430a82b68f4adbf7ba61a9e8bbfb2568
newonathe17 <ethanowen0117@gmail.com>
2025-05-11 21:14:40 +0800

    Simplify ArticleForm & CommentForm and remove as_widget filters from article_form.html

--- hobbysite/blog/forms.py
+++ hobbysite/blog/forms.py
@@ -1,23 +1,14 @@
+# blog/forms.py
 from django import forms
 from .models import Article, Comment
 
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        # author, created_on, updated_on are excluded
+        # We only need to declare which fields to include; widgets go in the template
         fields = ['title', 'category', 'entry', 'header_image']
-        widgets = {
-            'entry': forms.Textarea(attrs={'rows': 6, 'class': 'form-control'}),
-            'category': forms.Select(attrs={'class': 'form-select'}),
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'header_image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
-        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        # author, article, created_on, updated_on are excluded
         fields = ['entry']
-        widgets = {
-            'entry': forms.Textarea(attrs={'rows': 3, 'class': 'form-control'}),
-        }
