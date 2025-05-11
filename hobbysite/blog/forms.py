cf4895994165c8deebb4ac24f8c6288d6c97f3a4
newonathe17 <ethanowen0117@gmail.com>
2025-05-11 16:21:45 +0800

    Here’s a sentence-style commit message summarizing all the changes **except** views and URLs:
    > Added `author` and `header_image` fields to the `Article` model and created a new `Comment` model linked to both `Article` and `Profile`; registered the `Comment` model in the admin panel with search and filter options; and created `ArticleForm` and `CommentForm` in `forms.py` to handle article creation/editing and comment submissions using ModelForms with Bootstrap-compatible widgets.

---
+++ hobbysite/blog/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Article, Comment, ArticleCategory
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        # author, created_on, updated_on are excluded
+        fields = ['title', 'category', 'entry', 'header_image']
+        widgets = {
+            'entry': forms.Textarea(attrs={'rows': 6, 'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-select'}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'header_image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        # author, article, created_on, updated_on are excluded
+        fields = ['entry']
+        widgets = {
+            'entry': forms.Textarea(attrs={'rows': 3, 'class': 'form-control'}),
+        }
