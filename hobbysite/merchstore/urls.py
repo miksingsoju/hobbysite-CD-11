b01dd0d5004eeccf2bb4349122f387437dd43ffa
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-05-12 18:46:18 +0800

    Remove unnecessary files

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -9,11 +9,6 @@ urlpatterns = [
     path('item/<int:num>/edit/', product_update, name="product_update"),
     path('cart/',cart, name="cart"),
     path('transactions/', transactions, name="transactions"),
-
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-
-
     
 ]
 
