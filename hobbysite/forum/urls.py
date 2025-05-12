1140cdb847d10b7f4cd2a07d627e9fdb74236966
floodlight <go.miguel65@gmail.com>
2025-05-12 22:52:57 +0800

    fix new error with forum urls.py

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import thread_list, thread_detail, thread_update, thread_create
 
 urlpatterns = [
-    path('threads', thread_list, name='thread_list'),
+    path('threads/', thread_list, name='thread_list'),
     path('thread/<int:thread_id>/', thread_detail, name='thread_detail'),
-    path('thread/<int:thread_id>/edit', thread_update, name='thread_update'),
-    path('thread/add', thread_create, name="thread_create")
+    path('thread/<int:thread_id>/edit/', thread_update, name='thread_update'),
+    path('thread/add/', thread_create, name="thread_create")
 ]
 
 app_name = "forum"
