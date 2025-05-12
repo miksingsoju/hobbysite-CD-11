85d8ee0dd982356f8dc208b7642b532a7f909acb
amantenoah <111347849+amantenoah@users.noreply.github.com>
2025-05-12 23:46:59 +0800

    added a registration view and url

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.update_profile, name="update_profile")
+    path('', views.update_profile, name="update_profile"),
+    path('/register/', views.register, name="create_profile")
 ]
 
 app_name = "user_management"
--- hobbysite/user_management/views.py
+++ hobbysite/user_management/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from .forms import ProfileForm
-# for debugging only
-from django.http import HttpResponse
 
+@login_required
 def update_profile(request):
     profile = request.user.profile 
     
@@ -14,5 +15,31 @@ def update_profile(request):
             return redirect('homepage')
     else:
         form = ProfileForm(instance=profile)
-        
     return render(request, "update_profile.html", {'form': form})
+
+def register(request):
+    if request.method == 'POST':
+        # Create the user first
+        user_form = UserCreationForm(request.POST)
+        profile_form = ProfileForm(request.POST)
+        
+        if user_form.is_valid() and profile_form.is_valid():
+            # Save the user
+            user = user_form.save()
+            
+            # Create the profile linked to the user
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            
+            # Log the user in
+            login(request, user)
+            return redirect('homepage')
+    else:
+        user_form = UserCreationForm()
+        profile_form = ProfileForm()
+    
+    return render(request, 'registration/register.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
