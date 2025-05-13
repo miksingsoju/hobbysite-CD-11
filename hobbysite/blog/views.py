f2202cf88b2973d8473aa11e8a230f8193230b7f
newonathe17 <ethanowen0117@gmail.com>
2025-05-13 11:33:10 +0800

    fetching files

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Article, ArticleCategory
 from .forms import ArticleForm, CommentForm
